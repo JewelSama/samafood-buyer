d7230e20c4ce1e0df0de1d6a5c5734bfe4d9e8d9
JewelSama <osafilejewel@gmail.com>
2024-04-13 13:41:22 +0100

    app

--- App.tsx
+++ App.tsx
@@ -1,23 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Categories from './components/Categories';
-import FeaturedRow from './components/FeaturedRow';
 import { NavigationContainer } from '@react-navigation/native';
 import StackNavigator from './Navigators/StackNavigator';
+import * as SplashScreen from 'expo-splash-screen';
+import { useFonts } from 'expo-font';
+import { useCallback, useEffect } from 'react';
+import AuthStack from './Navigators/AuthStack';
+
+
+
+
+SplashScreen.preventAutoHideAsync();
+
 
 export default function App() {
+
+  
+	const [fontsLoaded] = useFonts({
+		'Regular': require('./assets/fonts/Nunito-Regular.ttf'),
+		'Medium': require('./assets/fonts/Nunito-Medium.ttf'),
+		'SemiBold': require('./assets/fonts/Nunito-SemiBold.ttf'),
+		'Bold': require('./assets/fonts/Nunito-Bold.ttf'),
+		'ExtraBold': require('./assets/fonts/Nunito-ExtraBold.ttf'),
+	});
+
+  	const onLayoutRootView = useCallback(async () => {
+		if (fontsLoaded) await SplashScreen.hideAsync();
+  	}, [fontsLoaded]);
+
+	useEffect(() => {
+		onLayoutRootView();
+	});
+
+	if (!fontsLoaded) return null;
+
   return (
 		<NavigationContainer>
-      <StackNavigator />
+      <AuthStack />
+        {/* <StackNavigator /> */}
     </NavigationContainer>
+    // <BasketScreen />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
