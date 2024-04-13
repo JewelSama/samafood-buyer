01408ba366a3b5bfc68c7532088940fd68f82506
JewelSama <osafilejewel@gmail.com>
2024-04-13 13:42:54 +0100

    scrn

--- Navigators/StackNavigator.tsx
+++ Navigators/StackNavigator.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from '../Screens/HomeScreen';
+import RestaurantScreen from '../Screens/RestaurantScreen';
+import BasketScreen from '../Screens/BasketScreen';
+import PreaparingOrderScreen from '../Screens/PreaparingOrderScreen';
+import DeliveryScreen from '../Screens/DeliveryScreen';
 
 
 
@@ -10,6 +14,16 @@ const StackNavigator = () => {
   return (
     <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Restaurant" component={RestaurantScreen} />
+        <Stack.Screen name="Basket" component={BasketScreen} 
+            options={{ presentation: "modal" , headerShown: false }}
+        />
+        <Stack.Screen name="PreparingOrderScreen" component={PreaparingOrderScreen} 
+          options={{presentation: "fullScreenModal", headerShown:false}}
+        />
+          <Stack.Screen name="Delivery" component={DeliveryScreen} 
+            options={{presentation: "fullScreenModal", headerShown:false}}
+          />
     </Stack.Navigator>
   )
 }
