112f6d6f03057a90243c76ab217183c0c1c32e35
JewelSama <osafilejewel@gmail.com>
2024-04-13 13:42:34 +0100

    comp.

--- components/BasketIcon.tsx
+++ components/BasketIcon.tsx
@@ -1,11 +1,12 @@
 import { View, Text, TouchableOpacity } from 'react-native'
-import React from 'react'
-import {useSelector} from 'react-redux'
-import { selectBasketItems, selectBasketTotal } from '../features/BasketSlice'
+import React, { useState } from 'react'
+import { useNavigation } from '@react-navigation/native'
 
-const BasketIcon = ({navigation} : any) => {
-    const items = useSelector(selectBasketItems)
-    const basketTotal = useSelector(selectBasketTotal)
+const BasketIcon = () => {
+  const navigation: any = useNavigation()
+    // const items = useSelector(selectBasketItems)
+    // const basketTotal = useSelector(selectBasketTotal)
+    const [items, setItems] = useState([5]) 
 
     if(items.length === 0) return null;
 
@@ -13,10 +14,10 @@ const BasketIcon = ({navigation} : any) => {
     <View className="absolute w-full bottom-10 z-50">
       <TouchableOpacity onPress={() => navigation.navigate('Basket')} className="bg-[#00cc88] mx-5 p-4 rounded-lg flex-row items-center space-x-1">
         <Text className="text-white font-extrabold text-lg bg-[#01A296] py-1 px-2">
-            {items.length}
+            5
         </Text>
         <Text className="flex-1 text-white font-extrabold text-lg text-center">View Basket</Text>
-        <Text className="text-lg text-white font-extrabold">₦{basketTotal}</Text>
+        <Text className="text-lg text-white font-extrabold">₦2800</Text>
       </TouchableOpacity>
     </View>
   )
