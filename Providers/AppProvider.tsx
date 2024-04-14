69d112d75949fd8964d7753f4812dfce45b39550
JewelSama <osafilejewel@gmail.com>
2024-04-14 16:10:24 +0100

    provider

---
+++ Providers/AppProvider.tsx
@@ -0,0 +1,17 @@
+import React, { createContext, useState } from 'react';
+
+
+export const AppContext = createContext({})
+
+export const AppProvider = ({children}: any) => {
+    const [ user, setUser ] = useState([])
+
+  return (
+    <AppContext.Provider value={{
+      user,
+      setUser
+    }}>
+        {children}
+    </AppContext.Provider>
+  )
+}
