702462e49885375a4d01b72301675d9405591296
JewelSama <osafilejewel@gmail.com>
2024-04-16 18:18:28 +0100

    add end points

--- endpoints.ts
+++ endpoints.ts
@@ -2,3 +2,5 @@ export const baseURL = 'https://jewel.cmsandbox.com.ng'
 export const RegisterAPI = 'https://jewel.cmsandbox.com.ng/api/buyer/register'
 export const LoginAPI = 'https://jewel.cmsandbox.com.ng/api/buyer/login'
 export const MenusAPI = 'https://jewel.cmsandbox.com.ng/api/buyer/menu/all'
+export const VendorsAPI = 'https://jewel.cmsandbox.com.ng/api/vendors/all'
+export const VendorMenuAPI = 'https://jewel.cmsandbox.com.ng/api/vendor/menu'  // -> /{id}
