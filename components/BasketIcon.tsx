5039f6679725c375f32b97f3b47f6c9e15f415ce
JewelSama <osafilejewel@gmail.com>
2024-04-17 17:20:16 +0100

    id

--- components/BasketIcon.tsx
+++ components/BasketIcon.tsx
@@ -3,7 +3,7 @@ import React, { useContext, useState } from 'react'
 import { useNavigation } from '@react-navigation/native'
 import { CartContext, useCart } from '../Providers/CartProvider'
 
-const BasketIcon = () => {
+const BasketIcon = ({ vendor_id }: any) => {
   const navigation: any = useNavigation()
   // @ts-ignore
     const { totalCartItems, cartItems, totalAmount } = useCart();
@@ -11,12 +11,11 @@ const BasketIcon = () => {
     // console.log('Cart: ', cartItems)
 
 
-
     if(totalCartItems === 0) return null;
 
   return (
     <View className="absolute w-full bottom-10 z-50">
-      <TouchableOpacity onPress={() => navigation.navigate('Basket')} className="bg-[#064929] mx-5 p-4 rounded-lg flex-row items-center space-x-1">
+      <TouchableOpacity onPress={() => navigation.navigate('Basket', {vendor_id})} className="bg-[#064929] mx-5 p-4 rounded-lg flex-row items-center space-x-1">
         <Text className="text-white font-extrabold text-lg bg-[#0f1e17] py-1 px-2">
             {totalCartItems}
         </Text>
