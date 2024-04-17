cbcea968005fcc5eb26d0d887b7145dcafa0f895
JewelSama <osafilejewel@gmail.com>
2024-04-17 17:19:56 +0100

    new endpoints

--- endpoints.ts
+++ endpoints.ts
@@ -3,4 +3,7 @@ export const RegisterAPI = 'https://jewel.cmsandbox.com.ng/api/buyer/register'
 export const LoginAPI = 'https://jewel.cmsandbox.com.ng/api/buyer/login'
 export const MenusAPI = 'https://jewel.cmsandbox.com.ng/api/buyer/menu/all'
 export const VendorsAPI = 'https://jewel.cmsandbox.com.ng/api/vendors/all'
+export const CreateOrderAPI = 'https://jewel.cmsandbox.com.ng/api/buyer/order/new'
+export const GetOrderAPI = 'https://jewel.cmsandbox.com.ng/api/buyer/orders'
 export const VendorMenuAPI = 'https://jewel.cmsandbox.com.ng/api/vendor/menu'  // -> /{id}
+export const PaystackKey = 'pk_test_4e4d75a86f6c6eb34aa43963d0d6d2179d3d0594'
