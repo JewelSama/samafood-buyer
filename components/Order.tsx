a8081f4de38ce4611f5808bc9de15f72529e8a2e
JewelSama <osafilejewel@gmail.com>
2024-04-17 17:21:52 +0100

    displ

--- components/Order.tsx
+++ components/Order.tsx
@@ -1,16 +1,32 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { dateTimeFormatter } from '../utils'
 
-const Order = () => {
+const Order = ({ order }: any) => {
+
   return (
     <View className='mb-1'>
     <View className='flex flex-row justify-between px-3 items-center h-24'>
       <View className='space-y-1'>
-        <Text style={{fontFamily: 'SemiBold'}} className='text-base'>Chillout Restaurant</Text>
-        <Text style={{fontFamily: 'Regular'}} className='text-slate-700'>23rdFeb, 2024.1:30</Text>
+        <Text style={{fontFamily: 'SemiBold'}} className='text-base'>{order?.vendor?.name}</Text>
+        <Text style={{fontFamily: 'Regular'}} className='text-slate-700'>{dateTimeFormatter(order?.created_at)}</Text>
       </View>
       <View className='items-center space-y-1'>
-        <Text style={{fontFamily: 'Regular'}} className='text-green-500'>Completed</Text>
+        {
+          order.status === "pending" && (
+            <Text style={{fontFamily: 'Regular'}} className='text-yellow-600'>{order.status.toUpperCase()}</Text>
+          )
+        }
+        {
+          order.status === "declined" && (
+            <Text style={{fontFamily: 'Regular'}} className='text-red-500'>{order.status.toUpperCase()}</Text>
+          )
+        }
+        {
+          order.status === "completed" && (
+            <Text style={{fontFamily: 'Regular'}} className='text-green-500'>{order.status.toUpperCase()}</Text>
+          )
+        }
         <Text style={{fontFamily: 'Regular'}} className='text-green-900'>View timeline</Text>
       </View>
     </View>
