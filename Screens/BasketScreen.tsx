35fdd5cf4d5e3d9ff9c861a87f9206e701c5f318
JewelSama <osafilejewel@gmail.com>
2024-04-17 13:51:35 +0100

    txt

--- Screens/BasketScreen.tsx
+++ Screens/BasketScreen.tsx
@@ -31,7 +31,7 @@ const BasketScreen = ({ navigation }: any) => {
 
         <View className="flex-row items-center space-x-4 px-4 py-3 bg-white my-5">
           <Image source={{uri: "https://links.papareact.com/wru"}} className="h-7 w-7 bg-gray-300 p-4 rounded-full" />
-          <Text className="flex-1" style={{fontFamily: 'Regular'}}>Delivery in 20-50 mins</Text>
+          <Text className="flex-1" style={{fontFamily: 'Regular'}}>Delivery in 20-50 minutes</Text>
           <TouchableOpacity>
             {/* <Text className="text-[#064929]">Change</Text> */}
           </TouchableOpacity>
