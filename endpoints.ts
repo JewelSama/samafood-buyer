f64d1691ec223c0fb60db2f09926c843db9d918a
JewelSama <osafilejewel@gmail.com>
2024-04-17 18:47:21 +0100

    add endpoint

--- endpoints.ts
+++ endpoints.ts
@@ -6,4 +6,5 @@ export const VendorsAPI = 'https://jewel.cmsandbox.com.ng/api/vendors/all'
 export const CreateOrderAPI = 'https://jewel.cmsandbox.com.ng/api/buyer/order/new'
 export const GetOrderAPI = 'https://jewel.cmsandbox.com.ng/api/buyer/orders'
 export const VendorMenuAPI = 'https://jewel.cmsandbox.com.ng/api/vendor/menu'  // -> /{id}
+export const UpdateProfileAPI = 'https://jewel.cmsandbox.com.ng/api/buyer/profile'  // -> /{id}
 export const PaystackKey = 'pk_test_4e4d75a86f6c6eb34aa43963d0d6d2179d3d0594'
